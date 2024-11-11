39e2f147c2f990b8325c9733adc192ecb9e96e5c
Gabriel Lopes <souzsgabriel12@gmail.com>
2024-11-11 16:19:56 -0300

    nova branch

--- roupas/views.py
+++ roupas/views.py
@@ -5,8 +5,8 @@ from .serializers import CategoriaSerializer, ProdutoSerializer
 from rest_framework import viewsets, filters
 from django_filters.rest_framework import DjangoFilterBackend
 class CategoriaViewSet(viewsets.ModelViewSet):
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+    # authentication_classes = [BasicAuthentication]
+    # permission_classes = [IsAuthenticated]
     queryset = Categoria.objects.all().order_by('nome')
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
     ordering_fields = ['nome']
@@ -14,8 +14,8 @@ class CategoriaViewSet(viewsets.ModelViewSet):
     serializer_class = CategoriaSerializer
 
 class ProdutoViewSet(viewsets.ModelViewSet):
-    authentication_classes = [ BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+    # authentication_classes = [ BasicAuthentication]
+    # permission_classes = [IsAuthenticated]
     queryset = Produto.objects.all().order_by('nome')
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
     ordering_fields = ['nome', 'preco']
