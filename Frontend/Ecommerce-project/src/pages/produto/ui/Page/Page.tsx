95daf612b686b334d53a8ef1b4effd117ea77ec9
Gabriel Lopes <souzsgabriel12@gmail.com>
2025-02-27 22:39:40 -0300

    Adicionar funcao de add um item no carrinho pela pagina do produto, sem duplicar o itemm

--- Frontend/Ecommerce-project/src/pages/produto/ui/Page/Page.tsx
+++ Frontend/Ecommerce-project/src/pages/produto/ui/Page/Page.tsx
@@ -2,6 +2,7 @@ import { FC, useEffect, useState } from "react"
 import { useParams } from "react-router-dom";
 import { fetchAuthApi } from "@/components";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 interface Produto {
   id: number;
@@ -22,27 +23,69 @@ interface dadosProduto {
   imagem: string;
 }
 
+interface ProdutosCarrinho {
+  perfil_carrinho: number;
+  produto: number;
+  quantidade: number;
+}
+
 const Produto: FC = () => {
   const [produto, setProduto] = useState<dadosProduto>({} as dadosProduto)
+  const [quantidade, setQuantidade] = useState(1);
   const {id_produto} = useParams<{ id_produto: string }>();
   const navigate = useNavigate();
+  const user_id = localStorage.getItem("user_id");
+  const [listaCarrinho, setListaCarrinho] = useState<ProdutosCarrinho[]>([])
 
   useEffect(() => {
     const fetchProduto = async () => {
       const refresh_token = localStorage.getItem('refresh_token')
       const produtoData = await fetchAuthApi(`${import.meta.env.VITE_URL}/produtos/${id_produto}/`, refresh_token, navigate)
-
       const cat_response = await fetchAuthApi(`${import.meta.env.VITE_URL}/categorias/${produtoData.categoria}/`, refresh_token, navigate);
       const categoria = await cat_response
 
-      
+      const listaCarrinhoData = await fetchAuthApi(`${import.meta.env.VITE_URL}/carrinhos/${user_id}/`, refresh_token, navigate );
+      setListaCarrinho(listaCarrinhoData);
       setProduto({
         ...produtoData,
-        categoria: categoria.nome, // Atribui o nome da categoria
+        categoria: categoria.nome, 
       });
     }
     fetchProduto()
-  }, [id_produto, navigate])
+  }, [id_produto, navigate, user_id, ])
+
+
+
+  const handleComprar = async () => {
+    console.log(`${user_id} ${id_produto} ${quantidade}`)
+    const matchingItem = listaCarrinho.find((item) => item.produto === parseInt(id_produto ?? ''));
+    if (matchingItem) {
+      const refresh_token = localStorage.getItem('access_token')
+      await axios.put(
+        `${import.meta.env.VITE_URL}carrinhos/${user_id}/${id_produto}/`,
+        {
+          'perfil_carrinho': user_id,
+          'produto': id_produto,
+          'quantidade': quantidade + matchingItem.quantidade
+        },
+        { headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${refresh_token}` } }
+      );
+      console.log('Produto atualizado no carrinho!')
+    } else {
+      const refresh_token = localStorage.getItem('access_token')
+    await axios.post(
+      `${import.meta.env.VITE_URL}carrinhos/${user_id}/`,
+      {
+        'perfil_carrinho': user_id,
+        'produto': id_produto,
+        'quantidade': quantidade
+      },
+      { headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${refresh_token}` } }
+    );
+    console.log('Produto adicionado ao carrinho!')
+    }
+    // navigate('/compras')
+  }
 
   return (
     <>
@@ -60,7 +103,19 @@ const Produto: FC = () => {
                   <p className='max-w-md text-justify' >{produto.descricao}</p>
                   <h1>{produto.categoria}</h1>
                   <h1 className="text-2xl font-bold"> {produto.preco}</h1>
-                  <button className="btn btn-primary my-2">Comprar</button>
+                  {/* Seção de quantidade e botão */}
+                  <div className="mt-auto flex items-center gap-4">
+                    {/* Input para quantidade */}
+                    <input 
+                      type="number" 
+                      min="1" 
+                      value={quantidade} 
+                      onChange={(e) => setQuantidade(parseInt(e.target.value))}
+                      className="w-16 text-center border border-gray-300 rounded-lg p-2"
+                    />
+                    {/* Botão de compra */}
+                    <button onClick={handleComprar} className="btn btn-primary">Comprar</button>
+                  </div>
                 </div>
             </div>
         </div>
